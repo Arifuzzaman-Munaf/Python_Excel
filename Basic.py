b32db2f8c87c6b9590ee62d68ab55b2c03c34e2c
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2021-08-08 14:42:47 +0600

    Updated

--- Basic.py
+++ Basic.py
@@ -2,5 +2,16 @@
 
 from openpyxl import  workbook,load_workbook
 
-wb = load_workbook('iris - iris.csv')
+# loading the workBook or Excel file
+wb = load_workbook('iris.xlsx')
 
+# loading the active workSheet
+ws = wb.active
+
+# to get the value of a cell
+print(ws['A1'].value)
+
+# to change the value of a cell
+ws['A1'].value = '10000'
+wb.save('iris.xlsx')
+print(ws['A1'].value)
